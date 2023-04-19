a62a0e5c91933556046421fe9447db0d2f757cda
Lucas Grillos <LucasGrillos@gmail.com>
2023-04-18 19:40:55 -0700

    added test code for library book cards

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${String(this.pages)}, ${this.read===true ? "has been read" : "not read yet"}`;
 }
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function renderBookCard() {
+
+}
+
+window.addEventListener("load", (event) => {
+    let book1 = new Book("The Hobbit", "JRR Tolkien", 304, false);
+    let book2 = new Book("Fuzzy Planet", "John Scalzi", 368, true);
+    let book3 = new Book("Stick and Rudder", "Wolfgang Langewische", 389, false);
+    addBookToLibrary(book1);
+    addBookToLibrary(book2);
+    addBookToLibrary(book3);
+
+    console.log(myLibrary);
+});
+  
